cd4968911406e1cd0ae84d752fbb778dfe3886db
deepakagashe18 <agashedeepak55@gmail.com>
2024-12-11 20:57:29 +0530

    reown done

--- reown-kit/app/page.tsx
+++ reown-kit/app/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Home
+    <div className='flex justify-center mt-60'>
+      <w3m-button />
     </div>
   )
 }
--- reown-kit/config/index.tsx
+++ reown-kit/config/index.tsx
@@ -3,11 +3,11 @@ import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { mainnet, arbitrum } from '@reown/appkit/networks'
 
 // Get projectId from https://cloud.reown.com
-export const projectId = "";
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 
-// if (!projectId) {
-//   throw new Error('Project ID is not defined')
-// }
+if (!projectId) {
+  throw new Error('Project ID is not defined')
+}
 
 export const networks = [mainnet, arbitrum]
 
